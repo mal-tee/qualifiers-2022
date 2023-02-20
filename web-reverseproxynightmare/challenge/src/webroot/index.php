13a44d1c1e4808f42a93a339a8c21a9d466714ff
dbhadmin <challenge@deutschlands-bester-hacker.de>
2023-02-20 10:12:25 +0100

    rpn - fix for local execution

--- web-reverseproxynightmare/challenge/src/webroot/index.php
+++ web-reverseproxynightmare/challenge/src/webroot/index.php
@@ -4,19 +4,19 @@ $mockFiles = array(
     "/robots.txt" => ["content" => "robots.txt.php"],
     "/s3cur3Fl4g.html" => ["content" => "s3cur3Fl4g.php"]
 );
-?>
-<?php if (sizeof(explode(",",$_SERVER["HTTP_X_FORWARDED_FOR"])) == 1): ?>
-    <?php include '../resc/error.php'; ?>
-<?php else: ?>
-    <?php if (ip2long($_SERVER["REMOTE_ADDR"]) >= 2886729729 && ip2long($_SERVER["REMOTE_ADDR"]) <= 2886729982) : ?>
-        <?php
+
+if (!isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+    include '../resc/error.php';
+}
+else{
+    if (ip2long($_SERVER["REMOTE_ADDR"]) >= 2886729729 && ip2long($_SERVER["REMOTE_ADDR"]) <= 2886729982) {
         if (isset($mockFiles[$_SERVER['REQUEST_URI']])) {
             include '../resc/' . $mockFiles[$_SERVER['REQUEST_URI']]['content'];
         } else {
             include '../resc/404.php';
         }
-        ?>
-    <?php else : ?>
-        <?php include '../resc/wrongCidr.php' ?>
-    <?php endif; ?>
-<?php endif; ?>
+    } else {
+        include '../resc/wrongCidr.php';
+    }
+}
+?>
